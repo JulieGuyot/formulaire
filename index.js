3cceb11a463f56983c28d0c4af1fe12d25650bdf
GUYOT Julie <guyotjulie@mbp-de-guyot.lan>
2020-07-17 17:19:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+const API_KEY = "process.env.API_KEY";
+const DOMAIN = "process.env.DOMAIN";
+const mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+app.post("/form", (req, res) => {
+  console.log(req.fields);
+
+  const data = {
+    from: "Excited User <me@samples.mailgun.org>",
+    to: "j.guyot_065@yahoo.fr",
+    subject: "Nouveau message ! ",
+    text: "Testing some Mailgun awesomeness!",
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  });
+
+  res.json({ message: "Données bien reçues" });
+});
+app.all("*", (req, res) => {
+  res.json({ message: "Page introuvable" });
+});
+
+app.listen(3001, () => {
+  console.log("Server started");
+});
